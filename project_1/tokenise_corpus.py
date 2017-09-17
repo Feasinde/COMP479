0d3c6c44f772e7360b71830cc5c7211f3a253d41
Andrés Lou <feasinde@gmail.com>
2017-09-17 15:04:36 -0400

    added tokenising feature

--- project_1/tokenise_corpus.py
+++ project_1/tokenise_corpus.py
@@ -7,9 +7,14 @@ list_of_files = []
 for file in os.listdir(PATH_TO_CORPUS):
 	if file.endswith(".sgm"): list_of_files.append(PATH_TO_CORPUS+file)
 
+tokens_file = open('tokens_file.txt','a')
 for path_to_filename in list_of_files:
 	print("currently at:",path_to_filename)
 	with open(path_to_filename) as sgm_file:
 		token_soup = BeautifulSoup(sgm_file,'html.parser')
 		list_of_articles = token_soup.find_all("reuters")
-		print(list_of_articles[0].text)
+		for article in list_of_articles:
+			tokens = nltk.word_tokenize(article.text)
+			# print(tokens)
+			# tokens_file.write(str(tokens))
+tokens_file.close()
